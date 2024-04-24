dbcedf543814bd603f2c6bf8c5a5ecdb80d25f0d
Oleksii Bidiak <oleksii.bidiak@gmail.com>
2024-04-24 18:46:30 +0300

    make build

---
+++ index.bbd36941.js
@@ -0,0 +1,2 @@
+let e=document.querySelector(".icon-menu");e&&e.addEventListener("click",function(e){document.documentElement.classList.toggle("menu-open")}),document.addEventListener("click",n=>{n.target!==e&&document.documentElement.classList.contains("menu-open")&&document.documentElement.classList.remove("menu-open")});
+//# sourceMappingURL=index.bbd36941.js.map
